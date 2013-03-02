f1f6eb7e276c386fab885ca4e4a6798fdb5bf2c2
Davy Chiu <davychiu@gmail.com>
2013-03-02 09:14:47 -0800

    temp static files

--- datedge/settings.py
+++ datedge/settings.py
@@ -4,6 +4,7 @@ import dj_database_url
 
 DEBUG = False 
 
+STATIC_PROVIDER = 'http://www.vcn.bc.ca/resume/datedge'
 PROJECT_PATH = os.path.join(os.path.dirname(__file__))
 
 ADMINS = (
@@ -49,7 +50,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = PROJECT_PATH + '/media/'
+MEDIA_ROOT = STATIC_PROVIDER + '/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +61,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = PROJECT_PATH + '/static/'
+STATIC_ROOT = STATIC_PROVIDER + '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
