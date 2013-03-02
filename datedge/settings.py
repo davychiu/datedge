f209c6cc13d5db72643189f383029d775891bdb9
Davy Chiu <davychiu@gmail.com>
2013-03-02 09:41:06 -0800

    temp static files fix5

--- datedge/settings.py
+++ datedge/settings.py
@@ -113,6 +113,15 @@ TEMPLATE_DIRS = (
     PROJECT_PATH + '/templates',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+)
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
