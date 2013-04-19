188a289851bbec687b1b83566d5d90a0408c8086
Davy Chiu <davychiu@gmail.com>
2013-04-18 19:00:32 -0700

    temp disable mailchimp

--- datedge/models.py
+++ datedge/models.py
@@ -21,8 +21,8 @@ class UserProfile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-        list = utils.get_connection().get_list_by_id('b4ae792bb9')
-        list.subscribe(instance.email, {'EMAIL': instance.email})
+        #list = utils.get_connection().get_list_by_id('b4ae792bb9')
+        #list.subscribe(instance.email, {'EMAIL': instance.email})
         send_mail('Welcome to DAT Edge', 'Welcome to your DAT Edge account!\n\n====================\nYour Login Details\n====================\n\nUsername: ' + instance.username + '\n\nLogin to start your Free Trial: http://www.datedge.com/accounts/login/\n\nIf you have any questions, please email us at support@datedge.com\n\nRegards,\nThe DAT Edge Team\nwww.datedge.com','support@datedge.com',[instance.email])
 
 signals.post_save.connect(create_user_profile, sender=User)
